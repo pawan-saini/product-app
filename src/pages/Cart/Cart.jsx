c522a6a833a8f3f01033e4f406fdec4bde394718
pawan-saini <pawansaini@Pawans-MacBook-Pro.local>
2024-01-08 07:26:03 +0530

    some layout changes

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -9,7 +9,14 @@ export default function CartPage() {
     <div className="container mx-auto mt-6 p-4">
       <h1 className="text-3xl font-semibold mb-4">Shopping Cart</h1>
       {cartItems.length === 0 ? (
-        <p>Your cart is empty.</p>
+        <>
+          <p>Your cart is empty.</p>
+          <Link to="/">
+            <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300">
+              Shop Now
+            </button>
+          </Link>
+        </>
       ) : (
         <div className="container mx-auto my-8">
           {cartItems.map((item) => (
--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -10,6 +10,7 @@ export default function Checkout() {
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [isCODSelected, setIsCODSelected] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -21,60 +22,83 @@ export default function Checkout() {
     navigate("/thankyou");
   };
 
+  const handleCODChange = () => {
+    setIsCODSelected(!isCODSelected);
+  };
+
   return (
-    <div className=" flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 shadow-md rounded-md w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-4">Checkout</h1>
+    <>
+      <div className="container mx-auto p-4 flex">
+        <div className="bg-white p-8 shadow-md rounded-md w-full max-w-md">
+          <h1 className="text-2xl font-bold mb-4">Checkout</h1>
 
-        <div className="mb-4">
-          <label className="block text-sm font-semibold mb-1">Name:</label>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="border rounded-md p-2 w-full"
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-sm font-semibold mb-1">Address:</label>
-          <textarea
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-            className="border rounded-md p-2 w-full"
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-sm font-semibold mb-1">
-            Phone Number:
-          </label>
-          <input
-            type="text"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            className="border rounded-md p-2 w-full"
-          />
-        </div>
-
-        <div className="mb-4 border p-4 rounded-md">
-          <h2 className="text-lg font-semibold mb-2">Order Summary</h2>
-          <div className="flex justify-between">
-            <span>Order Amount:</span>
-            <span>$100</span>
+          <div className="flex flex-col">
+            <label className="mb-2">
+              <input
+                type="checkbox"
+                className="mr-2"
+                checked={isCODSelected}
+                onChange={handleCODChange}
+              />
+              Pay with Cash on Delivery (COD)
+            </label>
           </div>
-          {/* You can add more summary items (e.g., taxes, discounts) here */}
-        </div>
-
-        <div className="mt-8">
-          <button
-            onClick={handlePlaceOrder}
-            className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300"
-          >
-            Place Order
-          </button>
+          {isCODSelected ? (
+            <>
+              <div className="mb-4">
+                <label className="block text-sm font-semibold mb-1">
+                  Name:
+                </label>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="border rounded-md p-2 w-full"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-semibold mb-1">
+                  Address:
+                </label>
+                <textarea
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                  className="border rounded-md p-2 w-full"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-semibold mb-1">
+                  Phone Number:
+                </label>
+                <input
+                  type="text"
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                  className="border rounded-md p-2 w-full"
+                />
+              </div>
+              <div className="mb-4 border p-4 rounded-md">
+                <h2 className="text-lg font-semibold mb-2">Order Summary</h2>
+                <div className="flex justify-between">
+                  <span>Order Amount:</span>
+                  <span>$100</span>
+                </div>
+                {/* You can add more summary items (e.g., taxes, discounts) here */}
+              </div>
+              <div className="mt-8">
+                <button
+                  onClick={handlePlaceOrder}
+                  className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300"
+                >
+                  Place Order
+                </button>
+              </div>
+            </>
+          ) : (
+            ""
+          )}
         </div>
       </div>
-    </div>
+    </>
   );
 }
